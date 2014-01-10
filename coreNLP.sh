5267749718c8cf82aab20c5f7d0a1d4bec63839b
Joost van Doorn <joost.van.doorn@gmail.com>
2014-01-10 14:17:56 +0100

    Updated coreNLP.sh

--- coreNLP.sh
+++ coreNLP.sh
@@ -1,3 +1,3 @@
 wget http://nlp.stanford.edu/software/stanford-corenlp-full-2014-01-04.zip -P lib/downloads
 
-unzip lib/downloads/stanford-corenlp-full-2014-01-04.zip -d lib/CoreNLP
+unzip lib/downloads/stanford-corenlp-full-2014-01-04.zip -d lib/
